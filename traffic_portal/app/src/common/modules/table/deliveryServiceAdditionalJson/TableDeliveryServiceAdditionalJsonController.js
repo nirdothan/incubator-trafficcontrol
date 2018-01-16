d4abe95b9031f36dc8051c51c5143f1285cac41b
Vitaly <vitalyb@gmail.com>
2018-01-16 21:21:43 +0200

    POC - jsondiff works

--- traffic_portal/app/src/common/modules/table/deliveryServiceAdditionalJson/TableDeliveryServiceAdditionalJsonController.js
+++ traffic_portal/app/src/common/modules/table/deliveryServiceAdditionalJson/TableDeliveryServiceAdditionalJsonController.js
@@ -428,6 +428,13 @@ var TableDeliveryServiceServersController = function(
     },
     true
   );
+
+  var left = { a: 3, b: 4 };
+  var right = { a: 5, c: 9 };
+  var delta = jsondiffpatch.diff(left, right);
+
+  // beautiful html diff
+  document.getElementById('visual').innerHTML = jsondiffpatch.formatters.html.format(delta, left);
 };
 
 TableDeliveryServiceServersController.$inject = [
